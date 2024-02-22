c03fc96303b51a9ecb96b3b80d27c29af0ed1d28
Igor <igorkozakiewicz13@gmail.com>
2024-02-22 20:45:09 +0000

    solved login issue for now by altering middleware, added styling for background and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs"
+
 export default function Home() {
   return (
-    <p>
-      Only autheticated users can see this
-    </p>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
